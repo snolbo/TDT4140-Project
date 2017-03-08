daa6ca58360108cc616c0b21ae60da5e6ecb372a
snolbo <snorreob@stud.ntnu.no>
2017-03-08 20:06:19 +0100

    The queueing system should now work as intended

--- connectED/src/communication/Connector.java
+++ connectED/src/communication/Connector.java
@@ -10,13 +10,13 @@ import T2.ServerRequest;
 // Either connect to or sets up connection depending on mode
 public class Connector implements Runnable {
 	private ServerSocket welcomeSocket = null; // sockets at server
-	private Queue<ChatController> controllerQueue;
 
-	private Boolean isHost;
+	private Boolean isHost = null;
 	private int hostPort;
+	private ChatTabController chatTabController;
 
-	public Connector() {
-		controllerQueue = new ArrayDeque<ChatController>();
+	public Connector(ChatTabController chatTabController) {
+		this.chatTabController = chatTabController;
 		this.hostPort = 9001; // port to connect to if client, port to open at if host
 	}
 
@@ -28,7 +28,7 @@ public class Connector implements Runnable {
 		this.isHost = true;
 		if(this.welcomeSocket == null){
 			try {
-				welcomeSocket = new ServerSocket(hostPort, 2);
+				welcomeSocket = new ServerSocket(hostPort, 20);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -38,44 +38,36 @@ public class Connector implements Runnable {
 	public void setClient() {
 		this.isHost = false;
 	}
-
+	
+	public void sendHelperRequest(){
+		ServerRequest request = new ServerRequest("Helper");
+		request.helperRequest();
+	}
+	
 	@Override
 	public void run() {
-		RecieveAndSend connection;
-		ChatController tempChatController = controllerQueue.poll();
-		Socket socket;
+		Socket socket = null;
 		if (isHost) {
-			tempChatController.setHost(true);
-			ServerRequest request = new ServerRequest("Helper");
-			request.helperRequest();
 			try {
 				socket = welcomeSocket.accept();
-				// TODO -> client should only be able to make one connection at a time
-				connection = new RecieveAndSend(socket, tempChatController);
-				new Thread(connection).start();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		else {
-			tempChatController.setHost(false);
 			ServerRequest request = new ServerRequest("Student");
 			String helperIP = request.studentRequest();
 			try {
-				socket = new Socket(helperIP, this.hostPort ); 
-				connection = new RecieveAndSend(socket, tempChatController); 
-				new Thread(connection).start();
+				socket = new Socket(helperIP,hostPort);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-	}
-
-	public void passChatTabController(ChatController controller) {
-		this.controllerQueue.add(controller);
+		if(socket != null)
+			this.chatTabController.startChatSession(socket);
 	}
 	
-	public void onCloseRequest(){
+	public void closeWelcomeSocket(){
 		try {
 			if(this.welcomeSocket != null)
 				this.welcomeSocket.close();
